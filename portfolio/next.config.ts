f6e20924030ab1b1e133f50fb6a3004d83a66db8
Avi-Bane <astroavichauhan@gmail.com>
2026-05-12 19:34:40 +0530

    Update next.config.ts

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export',
-  basePath: '/your-repo-name', // Only if not using a custom domain
-  images: { unoptimized: true }, // GitHub Pages doesn't support Next.js image optimization
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
